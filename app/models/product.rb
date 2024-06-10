b1540715081f66482f355928152e1b05d2d57636
mumbi <mumbi099@gmail.com>
2024-06-10 09:48:02 +0300

    Added validations for Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
+
+  validates :serial_number, presence: true, uniqueness: true
+  validates :category, :name, :date_bought, presence: true
+  validates :unit_price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :status, presence: true, inclusion: { in: ['Available', 'Assigned'] }
 end
