0cabc2f30652ca8b54aef73a4c33280526d8b0a2
Sylvia Mumbi <mumbi099@gmail.com>
2024-06-10 04:16:36 +0300

    Generated resource Product and ran migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_10_011318) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_10_011605) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,19 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_10_011318) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "products", force: :cascade do |t|
+    t.string "serial_number"
+    t.string "category"
+    t.string "name"
+    t.date "date_bought"
+    t.decimal "unit_price"
+    t.string "status"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -32,4 +45,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_10_011318) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "products", "users"
 end
