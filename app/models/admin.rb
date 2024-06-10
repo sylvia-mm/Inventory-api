88b1823e71ce7dfad822236da509d6aa0d2eb8f6
Sylvia Mumbi <mumbi099@gmail.com>
2024-06-10 03:25:02 +0300

    Corrected admin model mistake

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,5 +3,5 @@ class Admin < ApplicationRecord
   
     validates :first_name, :last_name, presence: true
     validates :email, presence: true, format: { with: /\A[^@\s]+@dakawou\.com\z/, message: "Use a valid DTL email address" }, uniqueness: true
-    alidates :password, presence: true, length: {minimum: 6 }
+    validates :password, presence: true, length: {minimum: 6 }
 end
