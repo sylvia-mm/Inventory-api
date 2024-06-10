2abab1e0be620f299b32fa1c69d31baab0c57b16
mumbi <mumbi099@gmail.com>
2024-06-10 14:24:07 +0300

    Added auth to ensure only admin -> crud actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
     rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
-
+    before_action :authorize
     before_action :find_product, only: [:show, :update, :destroy]
 
     def index
@@ -23,4 +23,7 @@ class ProductsController < ApplicationController
         params.require(:product).permit(:serial_number, :category, :name, :unit_price, :date_bought, :status, :user_id)
     end
 
+    def authorize
+        return head :no_content unless session[:admin_id]
+    end
 end
