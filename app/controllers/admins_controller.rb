e14e358c191eace6e82e3a0af7814ad2795eeee1
Sylvia Mumbi <mumbi099@gmail.com>
2024-06-10 03:53:07 +0300

    Added admin signup method

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,8 +1,22 @@
 class AdminsController < ApplicationController
     before_action :authorize, except: [:create]
   
+    def create
+      @admin = Admin.new(admin_params)
+      if @admin.save
+        session[:admin_id] = @admin.id
+        render json: @admin, status: :created
+      else
+        render json: @admin.errors, status: :unprocessable_entity
+      end
+    end
+  
     private
-
+  
+    def admin_params
+      params.require(:admin).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
+  
     def authorize
       return head :unauthorized unless session[:admin_id]
     end
