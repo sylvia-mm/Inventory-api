7d19df24f271a86e47ccebcaaa07b16bb3c94498
Sylvia Mumbi <mumbi099@gmail.com>
2024-06-10 03:27:28 +0300

    Implemented authorization check

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,4 +1,10 @@
 class AdminsController < ApplicationController
+    before_action :authorize, except: [:create]
+  
+    private
 
+    def authorize
+      return head :unauthorized unless session[:admin_id]
+    end
 end
   
