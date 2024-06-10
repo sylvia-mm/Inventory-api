d3aecd0947eea77e65e97e6dfe2ea7451688c33f
mumbi <mumbi099@gmail.com>
2024-06-10 10:03:37 +0300

    created skeletal structure for products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,33 @@
 class ProductsController < ApplicationController
+    before_action :authorize
+
+    def index
+
+    end
+
+    def show
+
+    end
+
+    def create
+      
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def find_product
+
+    end
+
+    def product_params
+        
+    end
 end
