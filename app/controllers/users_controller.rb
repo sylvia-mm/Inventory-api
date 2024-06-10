2e2b8795b30f541cbd56837c044f72ae7a1b6498
mumbi <mumbi099@gmail.com>
2024-06-10 12:36:53 +0300

    Updated user model validations and user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,11 @@ class UsersController < ApplicationController
 
     def create
         @user = User.create(user_params)
+        if @user.save
         render json: @user, status: :created
+        else 
+        render json: @user.errors, status: :unprocessable_entity
+        end
     end
 
     def update
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_many :products
+
+    CATEGORIES= ['Stores', 'IT', 'Maintenance', 'Operations', 'Drivers', 'Finance']
+    validates :department, presence: true, inclusion: { in: CATEGORIES, message: "Please use a valid category" }
+    validates :first_name, :last_name, presence: true
+    validates :email, presence: true, format: { with: /\A[^@\s]+@dakawou\.com\z/, message: "Use a valid DTL email address" }, uniqueness: true
 end
