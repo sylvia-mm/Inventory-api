4740ba2796e7ce849aebbf8cddc9f7c6b8a40b54
Sylvia Mumbi <mumbi099@gmail.com>
2024-06-10 03:57:58 +0300

    Added sessions method to login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,12 @@
 class SessionsController < ApplicationController
- 
+  def create
+    @admin = Admin.find_by(email: params[:email])
+    if @admin&.authenticate(params[:password])
+      session[:admin_id] = @admin.id
+      render json: @admin, status: :ok
+    else
+      render json: { error: 'Incorrect email or password' }, status: :unauthorized
+    end
+  end
+
 end
