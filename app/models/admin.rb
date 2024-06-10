dc5a5fc78b4826fad73a85e9d47328a42f467da0
Sylvia Mumbi <mumbi099@gmail.com>
2024-06-10 03:22:37 +0300

    Updated admin attribute validations

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,7 @@
 class Admin < ApplicationRecord
     has_secure_password
   
-    validates :email, presence: true, uniqueness: true
     validates :first_name, :last_name, presence: true
+    validates :email, presence: true, format: { with: /\A[^@\s]+@dakawou\.com\z/, message: "Use a valid DTL email address" }, uniqueness: true
+    alidates :password, presence: true, length: {minimum: 6 }
 end
