5d25b50f35cb4385445885d96cd8c027711037d0
mumbi <mumbi099@gmail.com>
2024-07-26 16:30:26 +0300

    Model and controller updates to allow selected emails to sign up

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,24 +1,49 @@
+# class AdminsController < ApplicationController
+#     before_action :authorize, except: [:create]
+  
+#     def create
+#       @admin = Admin.new(admin_params)
+#       if @admin.save
+#         session[:admin_id] = @admin.id
+#         render json: @admin, status: :created
+#       else
+#         render json: @admin.errors, status: :unprocessable_entity
+#       end
+#     end
+  
+#     private
+  
+#     def admin_params
+#       params.require(:admin).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+#     end
+  
+#     def authorize
+#       return head :unauthorized unless session[:admin_id]
+#     end
+# end
+
 class AdminsController < ApplicationController
-    before_action :authorize, except: [:create]
-  
-    def create
-      @admin = Admin.new(admin_params)
-      if @admin.save
-        session[:admin_id] = @admin.id
-        render json: @admin, status: :created
-      else
-        render json: @admin.errors, status: :unprocessable_entity
-      end
+  before_action :authorize, except: [:create]
+
+  def create
+    @admin = Admin.new(admin_params)
+    if @admin.save
+      session[:admin_id] = @admin.id
+      render json: @admin, status: :created
+    else
+      render json: @admin.errors, status: :unprocessable_entity
     end
-  
-    private
-  
-    def admin_params
-      params.require(:admin).permit(:first_name, :last_name, :email, :password, :password_confirmation)
-    end
-  
-    def authorize
-      return head :unauthorized unless session[:admin_id]
-    end
+  end
+
+  private
+
+  def admin_params
+    params.require(:admin).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
+  def authorize
+    return head :unauthorized unless session[:admin_id]
+  end
 end
+
   
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,7 +1,26 @@
-class Admin < ApplicationRecord
-    has_secure_password
+# class Admin < ApplicationRecord
+#     has_secure_password
   
-    validates :first_name, :last_name, presence: true
-    validates :email, presence: true, format: { with: /\A[^@\s]+@dakawou\.com\z/, message: "Use a valid DTL email address" }, uniqueness: true
-    validates :password, presence: true, length: {minimum: 6 }
+#     validates :first_name, :last_name, presence: true
+#     validates :email, presence: true, format: { with: /\A[^@\s]+@dakawou\.com\z/, message: "Use a valid DTL email address" }, uniqueness: true
+#     validates :password, presence: true, length: {minimum: 6 }
+# end
+
+class Admin < ApplicationRecord
+  has_secure_password
+
+  validates :first_name, :last_name, presence: true
+  validates :email, presence: true, format: { with: /\A[^@\s]+@dakawou\.com\z/, message: "Use a valid DTL email address" }, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validate :email_in_authorized_list
+
+  AUTHORIZED_USERS = ['it@dakawou.com', 'washington@dakawou.com', 'itsupport@dakawou.com', 'sylvia.mumbi@dakawou.com']
+
+  private
+
+  def email_in_authorized_list
+    unless AUTHORIZED_USERS.include?(email)
+      errors.add(:email, 'is not authorized to sign up')
+    end
+  end
 end
