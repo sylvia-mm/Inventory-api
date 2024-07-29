7b7bb30f6eff6335d6e40c3de1ba6ea5a5dcb4cb
mumbi <mumbi099@gmail.com>
2024-07-29 12:02:05 +0300

    Removed commented section

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,4 @@
 class SessionsController < ApplicationController
-  # def create
-  #   @admin = Admin.find_by(email: params[:email])
-  #   if @admin&.authenticate(params[:password])
-  #     session[:admin_id] = @admin.id
-  #     render json: @admin, status: :ok
-  #   else
-  #     render json: { error: 'Incorrect email or password' }, status: :unauthorized
-  #   end
-  # end
   def create
     admin = Admin.find_by(email: params[:email])
     
