7f06f7a81ede85a5c015e9bfc33a5ced80493b58
mumbi <mumbi099@gmail.com>
2024-08-20 16:48:27 +0300

    update

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,11 +1,3 @@
-# class Admin < ApplicationRecord
-#     has_secure_password
-  
-#     validates :first_name, :last_name, presence: true
-#     validates :email, presence: true, format: { with: /\A[^@\s]+@dakawou\.com\z/, message: "Use a valid DTL email address" }, uniqueness: true
-#     validates :password, presence: true, length: {minimum: 6 }
-# end
-
 class Admin < ApplicationRecord
   has_secure_password
 
