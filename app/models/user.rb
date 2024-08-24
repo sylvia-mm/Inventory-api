f80dd02fc4a9bdf0c43b8248fa2d947125cfcc5e
mumbi <mumbi099@gmail.com>
2024-08-24 14:43:32 +0300

    Add more dept categories

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_many :products, dependent: :nullify
 
-    CATEGORIES= ['Stores', 'IT', 'Maintenance', 'Operations', 'Drivers', 'Finance']
+    CATEGORIES = ['Stores', 'IT', 'Maintenance', 'Operations', 'Drivers', 'Finance', 'HSSE', 'HR']
     validates :department, presence: true, inclusion: { in: CATEGORIES, message: "Please use a valid category" }
     validates :first_name, :last_name, presence: true
     validates :email, presence: true, format: { with: /\A[^@\s]+@dakawou\.com\z/, message: "Use a valid DTL email address" }, uniqueness: true
