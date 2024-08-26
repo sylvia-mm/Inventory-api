52e19a1c30ed1310ae8b111cf04224e2d7b4f4b0
mumbi <mumbi099@gmail.com>
2024-08-26 15:33:11 +0300

    Link update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,7 +16,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001', 'http://localhost:3002', 'http://stocknest.dakawou.com/'
+    origins 'http://localhost:3001', 'http://localhost:3002', 'http://stocknest.dakawou.com'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
