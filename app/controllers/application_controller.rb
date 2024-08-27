e26edeb22778544ea1a1cff3561417d41c8585d0
mumbi <mumbi099@gmail.com>
2024-08-27 16:34:34 +0300

    Application controller update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::API
+  include ActionController::Cookies
+
+  before_action :require_login
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_login
+    render json: { message: 'Unauthorized' }, status: :unauthorized unless logged_in?
+  end
 end
