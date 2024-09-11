3ba01cc9abaabbd0d22a3128ccb62bdb12003789
mumbi <mumbi099@gmail.com>
2024-09-11 11:30:55 +0300

    include csrf token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,31 @@
+# class ApplicationController < ActionController::API
+#   include ActionController::Cookies
+# end
+# class ApplicationController < ActionController::Base
+#     protect_from_forgery with: :exception
+#     before_action :require_login
+  
+#     helper_method :current_admin, :logged_in?
+  
+#     private
+  
+  #   def current_user
+  #     @current_admin ||= User.find(session[:admin_id]) if session[:admin_id]
+  #   end
+  
+  #   def logged_in?
+  #     !!current_admin
+  #   end
+  
+  #   def require_login
+  #     unless logged_in?
+  #       render json: { status: 'error', message: 'Unauthorized' }, status: :unauthorized
+  #     end
+  #   end
+  # end
+
 class ApplicationController < ActionController::API
-  include ActionController::Cookies
+  protect_from_forgery unless: -> { request.format.json? }
 end
+
+  
