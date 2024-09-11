7a78d80b20b6d0375bdd101b8b44d5234300fcb5
mumbi <mumbi099@gmail.com>
2024-09-11 10:46:36 +0300

    Session store update

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,3 @@
-# config/initializers/session_store.rb
-Rails.application.config.session_store :cookie_store, key: '_inventory_hub_session', same_site: :none, secure: false
+# # config/initializers/session_store.rb
+# Rails.application.config.session_store :cookie_store, key: '_inventory_hub_session', same_site: :none, secure: false
+Rails.application.config.session_store :cookie_store, key: '_inventory_hub_session', same_site: :none, secure: false, domain: :all, tld_length: 2
