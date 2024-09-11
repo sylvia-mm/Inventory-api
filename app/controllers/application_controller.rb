38114c981da0ded8cfc726c62aecf77355feead5
mumbi <mumbi099@gmail.com>
2024-09-11 12:05:16 +0300

    Revert

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,3 @@
-# class ApplicationController < ActionController::API
-#   include ActionController::Cookies
-# end
-
 class ApplicationController < ActionController::API
-  skip_before_action :verify_authenticity_token
+  include ActionController::Cookies
 end
