e0175f5a3f7e4ed6b2f9cb6b3cc9c7dc0a0ca611
mumbi <mumbi099@gmail.com>
2024-09-11 11:40:14 +0300

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,7 @@
   # end
 
 class ApplicationController < ActionController::API
-  protect_from_forgery unless: -> { request.format.json? }
+   skip_before_action :verify_authenticity_token
 end
 
   
